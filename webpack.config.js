0162fb755db96ccfe6c34354ccb957064b33cc95
Emiliano Sanchez <emi042003@gmail.com>
2019-09-05 02:18:06 -0300

    Fixed min.js dist, by adding output.library and output.libraryTarget to Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "adversarial-search.min.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    library: "adversarialsearch",
+    libraryTarget: "umd"
   },
   module: {
     rules: [
